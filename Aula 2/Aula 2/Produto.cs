2f4c6548b7dee105e4ac0591866282b15b7ee19d
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-08-10 21:13:51 -0300

    Aula 2

---
+++ Aula 2/Aula 2/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2
+{
+    public class Produto
+    {
+        string nome;
+        List<Consulta> consultas;
+        
+    }
+}
---
+++ Aula 2/Aula 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Aula_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Leitura e escritade arquivos
+            string caminhoArquivo = "C:/Users/autologon.CSED/Documents/Visual Studio 2019/Aula 2/aula1Dados.csv"; //Caminho do Arqiuvo
+            if (File.Exists(caminhoArquivo))
+            {
+                Stream file = File.Open(caminhoArquivo, FileMode.Open);
+                StreamReader leitor = new StreamReader(file);
+                string linha = leitor.ReadLine();
+
+                while (linha != null)
+                {
+                    //Criar um produto por linha
+                    //
+                    List<Produto> produtos = new List<Produto>();
+                    Console.WriteLine(linha);
+                    linha = leitor.ReadLine();
+                }
+                leitor.Close();
+                file.Close();
+
+
+            }
+            else
+            {
+                Console.WriteLine("Nenhum arquivo encontrado");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
